82b3837c68cc892406a5d51d3a6913d26361436e
DHC <a601767890@sina.com>
2020-06-14 23:40:52 +0800

    修改编译后的静态资源路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     devServer: {
-        port: 8080,
-        disableHostCheck: true
-    }
+        port: 8080,  // dev模式的端口
+        disableHostCheck: true  // host请求头的检测，设置为true可以在公网服务器上使用dev模式
+    },
+    publicPath: '/static/learning_logs',  // 打包后的静态路径
+    // 没有书写outputDir属性   默认'dist'  对应dev.assetsSubDirectory.
+    // outputDir: 'dist/static',
 };
