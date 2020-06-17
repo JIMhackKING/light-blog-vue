6c610c7d77f3ba054b936adac7fda7579f33b7db
DHC <a601767890@sina.com>
2020-06-17 21:20:00 +0800

    将生成的静态资源集成到homepage项目中

--- vue.config.js
+++ vue.config.js
@@ -5,17 +5,21 @@ module.exports = {
     },
     publicPath: '/static/learning_logs',  // 打包后的静态路径
     // 没有书写outputDir属性   默认'dist'  对应dev.assetsSubDirectory.
-    // outputDir: 'dist/static',
+    outputDir: '../static/learning_logs',
     productionSourceMap: false,
+    // 删除 HTML 相关的 webpack 插件
+    chainWebpack: config => {
+        config.plugins.delete('html')
+        config.plugins.delete('preload')
+        config.plugins.delete('prefetch')
+    //     // // 清除css，js版本号
+    //     // config.output.filename('js/[name].js');
+    //     // config.output.chunkFilename('js/[name].js');
+    //     // // 为生产环境修改配置...
+    //     // config.plugin('extract-css').tap(args => [{
+    //     //     filename: `css/[name].css`,
+    //     //     chunkFilename: `css/[name].css`
+    //     // }])
+    },
     filenameHashing: false,
-    // chainWebpack: config => {
-    //     // 清除css，js版本号
-    //     config.output.filename('js/[name].js');
-    //     config.output.chunkFilename('js/[name].js');
-    //     // 为生产环境修改配置...
-    //     config.plugin('extract-css').tap(args => [{
-    //         filename: `css/[name].css`,
-    //         chunkFilename: `css/[name].css`
-    //     }])
-    // }
 };
